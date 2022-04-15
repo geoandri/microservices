ed6dc10ac8cba1bf78be66b1adcd19d9ccf5ecf8
Georgios Andrinopoulos <georgios.andrinopoulos@atos.net>
2022-04-15 19:56:40 +0300

    Add logger to deserializer

--- developers-service/src/test/java/org/geoandri/developers/consumer/TeamEventDeserializer.java
+++ developers-service/src/test/java/org/geoandri/developers/consumer/TeamEventDeserializer.java
@@ -6,10 +6,13 @@ import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.header.Headers;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.geoandri.developers.event.TeamEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 public class TeamEventDeserializer implements Deserializer<TeamEvent> {
+    private static Logger LOGGER = LoggerFactory.getLogger(TeamEventDeserializer.class);
     private ObjectMapper objectMapper = new ObjectMapper();
     @Override
     public void configure(Map<String, ?> configs, boolean isKey) {
@@ -26,6 +29,7 @@ public class TeamEventDeserializer implements Deserializer<TeamEvent> {
             System.out.println("Deserializing...");
             return objectMapper.readValue(new String(bytes, "UTF-8"), TeamEvent.class);
         } catch (Exception e) {
+            LOGGER.info(e.getMessage());
             throw new SerializationException("Error when deserializing byte[] to MessageDto");
         }
     }
